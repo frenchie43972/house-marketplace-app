e4c3090cd06390beb0584b83c211970422d1f66d
Kris French <frenchie43972@gmail.com>
2023-02-28 16:42:15 +0900

    building out the page templates

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div >
-      <h1> New App</h1>
-    </div>
+    < >
+    <Router>
+      <Routes>
+        <Route path="/" element={<Explore/>} />
+        <Route path="/forgot-password" element={<ForgotPassword/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/offers" element={<Offers/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+      </Routes>
+      <Navbar />
+    </Router>
+    </>
   );
 }
 
