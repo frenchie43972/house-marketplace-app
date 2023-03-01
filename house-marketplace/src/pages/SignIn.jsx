5d03cbf767688bf143b83cbd099ae777f434a54c
French <1078924850117002@mil>
2023-03-01 13:48:53 +0900

    blaah

--- house-marketplace/src/pages/SignIn.jsx
+++ house-marketplace/src/pages/SignIn.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 function SignIn() {
-    return (
-      <>
-        <h1>Sign In</h1>
-            
-      </>
-    );
-  }
+  const [showPassowrd, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+      </div>
+    </>
+  );
+ 
+}
   
-  export default SignIn;
+export default SignIn;
   
