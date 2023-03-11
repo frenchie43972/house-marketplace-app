14675818ccfc96d6c90b8c7d158d6b60b07d7532
Kris French <frenchie43972@gmail.com>
2023-03-12 08:04:54 +0900

    l

--- house-marketplace/src/components/ListingItem.jsx
+++ house-marketplace/src/components/ListingItem.jsx
@@ -7,7 +7,7 @@ function ListingItem({listing, id}) {
   return (
     <li className="categoryListing">
       <Link to={`/category/${listing.type}/${id}`} className="categoryListingLink">
-        <img src={listing.imgURLs[0]} alt={listing.name} className="categoryListingImg" />
+        <img src={listing.imageUrls[0]} alt={listing.name} className="categoryListingImg" />
         <div className="categoryListingDetails">
           <p className="categoryListingLocation">
             {listing.location}
@@ -16,12 +16,30 @@ function ListingItem({listing, id}) {
             {listing.name}
           </p>
           <p className="categoryListingPrice">
-            ${listing.offer
+            $
+            {listing.offer
                 ? listing.discountedPrice
                   .toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                : listing.regularPrice}
+                : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            {listing.type === 'rent' && ' / Month'}
           </p>
+          <div className="categoryListingInfoDiv">
+            <img src={bedIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bedrooms > 1 
+                ? `${listing.bedrooms} Bedrooms` 
+                : '1 Bedroom'}
+            </p>
+            <img src={bathtubIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bathrooms > 1 
+                ? `${listing.bathrooms} Bathrooms` 
+                : '1 Bathroom'}
+            </p>
+          </div>
         </div>
       </Link>
     </li>
